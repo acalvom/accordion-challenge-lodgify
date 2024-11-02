2e069cc0e0709866a61b667802dc1a2d6f1107aa
acalvom <andreac.3108@gmail.com>
2024-11-02 18:35:28 +0100

    feat: add controller to get tasks

---
+++ src/modules/task/ui/controllers/use-task-list.hook.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import { TaskLocator } from '../di/task.locator.ts'
+import { Group } from '@/modules/task/domain/group.ts'
+
+export function useTaskList() {
+  const [tasks, setTasks] = useState<Group[]>([])
+  const [isFetching, setIsFetching] = useState(true)
+
+  const getTasks = async () => {
+    const getTasksQuery = TaskLocator.getTasksQuery()
+    const tasks = await getTasksQuery.execute()
+
+    setTasks(tasks)
+    setIsFetching(false)
+  }
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
+  return { tasks, hasTasks: tasks.length > 0, isFetching }
+}
