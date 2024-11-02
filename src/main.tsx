7e2ddc084fc97e80ba91611f41e0474cc5b42be6
acalvom <andreac.3108@gmail.com>
2024-11-02 17:27:24 +0100

    style: add classnames to theme app

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Home } from '@/pages/home.page'
-import './index.css'
+import '@/core/styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
