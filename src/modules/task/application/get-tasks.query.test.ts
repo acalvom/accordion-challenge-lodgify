d204028fa9e9de900976f89bf8d383a1e0bb34e9
acalvom <andreac.3108@gmail.com>
2024-11-05 14:18:36 +0100

    test: fix type in test

--- src/modules/task/application/get-tasks.query.test.ts
+++ src/modules/task/application/get-tasks.query.test.ts
@@ -2,13 +2,11 @@ import { describe, expect, it, vi } from 'vitest'
 import { Group } from '@/modules/task/domain/group.ts'
 import { TaskRepository } from '@/modules/task/domain/task.repository.ts'
 import { GetTasksQuery } from '@/modules/task/application/get-tasks.query.ts'
+import { GroupMother } from '@/modules/task/domain/test/group.mother.ts'
 
 describe('GetTasksQuery', () => {
   it('should call the repository and return a list of groups', async () => {
-    const groups: Group[] = [
-      { id: '1', name: 'Group 1', tasks: [] },
-      { id: '2', name: 'Group 2', tasks: [] },
-    ]
+    const groups: Group[] = GroupMother.list()
 
     const taskRepository: TaskRepository = {
       getAll: vi.fn().mockResolvedValue(groups),
